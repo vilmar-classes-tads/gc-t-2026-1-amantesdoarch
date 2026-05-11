beddc7cc0754439615dd8f5e46945707f6c849c8
Gilberto Silva <gilbertozn527@gmail.com>
2026-05-11 19:22:19 -0300

    setup: configura projeto spring boot

---
+++ src/test/java/br/edu/ifpe/sistema_editais/SistemaEditaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.sistema_editais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEditaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

15567f12c50524c54a882d0de9caf64c4060aa36
Gilberto Silva <gilbertozn527@gmail.com>
2026-05-11 22:34:00 -0300

    feat: implementa o controlador User e o DTO UserRegistration para cadastro de usuários

---
+++ src/main/java/br/edu/ifpe/sistema_editais/controller/UserController.java
@@ -0,0 +1,36 @@
+package br.edu.ifpe.sistema_editais.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifpe.sistema_editais.dto.UserRegistrationDto;
+import br.edu.ifpe.sistema_editais.entity.User;
+import br.edu.ifpe.sistema_editais.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    
+    @PostMapping
+    public ResponseEntity<String> cadastrar(@RequestBody UserRegistrationDto dto) {
+        try {
+            userService.cadastrar(dto);
+            return ResponseEntity.ok().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}

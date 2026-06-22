444db436159c4e0dd67d7deb0b3d435a24354e5a
Gilberto Silva <gilbertozn527@gmail.com>
2026-06-22 18:27:25 -0300

    feat: termina implementação do metodo editarProjeto e implementa metodo criarProjeto

--- src/main/java/br/edu/ifpe/sistema_editais/service/ProjetoService.java
+++ src/main/java/br/edu/ifpe/sistema_editais/service/ProjetoService.java
@@ -2,6 +2,7 @@ package br.edu.ifpe.sistema_editais.service;
 
 import org.springframework.stereotype.Service;
 
+import br.edu.ifpe.sistema_editais.dto.ProjetoDto;
 import br.edu.ifpe.sistema_editais.entity.Projeto;
 import br.edu.ifpe.sistema_editais.repository.ProjetoRepository;
 
@@ -14,12 +15,33 @@ public class ProjetoService {
         this.projetoRepository = projetoRepository;
     }
 
-    public void editarProjeto(Projeto projeto) {
+    public void criarProjeto(ProjetoDto dto) {
+        Projeto p = new Projeto();
+        p.setTitulo(dto.titulo());
+        p.setPalavrasChave(dto.palavrasChave());
+        p.setCampus(dto.campus());
+        p.setAreaTematica(dto.areaTematica());
+        p.setOds(dto.ods());
+        p.setPublicoAlvo(dto.publicoAlvo());
+        p.setTermoDeCompromissoAceito(dto.termoDeCompromissoAceito());
+        p.setEstado("Rascunho");
+        projetoRepository.save(p);
+    }
+
+    public void editarProjeto(ProjetoDto dto) {
+        Projeto projeto = projetoRepository.getReferenceById(dto.id());
 
         if (!projeto.getEstado().equals("rascunho") && !projeto.getEstado().equals("em correção")) {
             throw new IllegalStateException("O projeto só pode ser editado se estiver em estado 'rascunho' ou 'em correção'");
         }
 
+        projeto.setOds(dto.ods());
+        projeto.setPublicoAlvo(dto.publicoAlvo());
+        projeto.setPalavrasChave(dto.palavrasChave());
+        projeto.setTermoDeCompromissoAceito(dto.termoDeCompromissoAceito());
+        projeto.setTitulo(dto.titulo());
+        projeto.setAreaTematica(dto.areaTematica());
+        projeto.setCampus(dto.campus());
         projetoRepository.save(projeto);
     }
 
